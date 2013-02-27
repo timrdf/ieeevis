77bd222f62e916f47b09bd745489dc4a488e51ea
Tim Lebo <timrdf@gmail.com>
2013-02-27 19:16:50 +0000

    installer source me backlog

---
+++ data/source/csv2rdf4lod-source-me-as-lebot.sh
@@ -0,0 +1,10 @@
+#3> <#> a <http://purl.org/twc/vocab/conversion/CSV2RDF4LOD_environment_variables> ;
+#3>     rdfs:seeAlso 
+#3>     <http://purl.org/twc/page/csv2rdf4lod/distributed_env_vars>,
+#3>     <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Script:-source-me.sh> .
+
+export CSV2RDF4LOD_CONVERT_PERSON_URI="http://tw.rpi.edu/instances/TimLebo"
+alias ieeevis='sudo su ieeevis'
+export PATH=$PATH`/home/lebot/opt/prizms/bin/install/paths.sh`
+export CLASSPATH=$CLASSPATH`/home/lebot/opt/prizms/bin/install/classpaths.sh`
+export CSV2RDF4LOD_HOME="/home/lebot/opt/prizms/repos/csv2rdf4lod-automation"
