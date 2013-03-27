58da74c0beced047af1b2e1a5f99219f5d25da0c
Tim Lebo <timrdf@gmail.com>
2013-03-27 22:29:55 +0000

    auto depicts.sh from 2-categories svg

--- data/source/ieeevis-tw-rpi-edu/lam-2012-evaluations-2-categories/version/2013-Mar-21/2manual.sh
+++ data/source/ieeevis-tw-rpi-edu/lam-2012-evaluations-2-categories/version/2013-Mar-21/2manual.sh
@@ -1,2 +1,4 @@
-file="lodspeakr-basic-menu.svg"
-depicts.sh -w -od manual $file --start-to --follow sio:has-member dcterms:subject skos:broader skos:broader skos:broader | tee manual/`basename $file`.ttl.out
+#!/bin/bash
+
+file="source/lodspeakr-basic-menu.svg"
+depicts.sh -w -od manual $file --start-to --follow sio:has-member dcterms:subject skos:broader skos:broader skos:broader 2>&1 | tee manual/`basename $file`.ttl.out
--- data/source/ieeevis-tw-rpi-edu/lam-2012-evaluations-2-categories/version/2manual.sh
+++ data/source/ieeevis-tw-rpi-edu/lam-2012-evaluations-2-categories/version/2manual.sh
@@ -1,2 +1,2 @@
 file="source/ScenarioPaperopencodingcleancopy.txt.xml.ttl.v6.graffle"
-depicts.sh -w -od manual $file --start-to --follow sio:has-member dcterms:subject skos:broader skos:broader skos:broader | tee manual/`basename $file`.ttl.out
+depicts.sh -w -od manual $file --start-to --follow sio:has-member dcterms:subject skos:broader skos:broader skos:broader 2>&1 | tee manual/`basename $file`.ttl.out
