4a27038932fe94b15dfd2c204fb8191648e6f378
Tim Lebo <timrdf@gmail.com>
2013-05-02 19:12:31 +0000

    external lodspeakr modules

--- lodspeakr/settings.inc.php
+++ lodspeakr/settings.inc.php
@@ -10,7 +10,24 @@ $conf['ns']['local']   = 'http://ieeevis.tw.rpi.edu/';
 
 $conf['mirror_external_uris'] = false;
 
-# https://github.com/timrdf/prizms/issues/12
+// Cherry-picked components (see https://github.com/alangrafu/lodspeakr/wiki/Reuse-cherry-picked-components-from-other-repositories)
+$conf['components']['types'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:VersionedDataset';
+$conf['components']['types'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/rdfs:Resource';
+$conf['components']['types'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:MetaDataset';
+// $conf['components']['types'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/old.conv';
+$conf['components']['types'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/void:Dataset';
+$conf['components']['types'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:AbstractDataset';
+$conf['components']['types'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:LayerDataset';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/vocabularies';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/ckan-mirror-status';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/sanity-checks';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/dataset-sample-graphs';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/search';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/instances';
+// $conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/tim';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/triples';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/datasets';
+$conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/classes';
 $conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/namedGraphs';
 $conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/graph';
 $conf['components']['services'][] = '/home/ieeevis/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/statistics';
