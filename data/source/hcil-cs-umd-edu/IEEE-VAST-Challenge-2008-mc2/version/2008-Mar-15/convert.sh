a153ba684097bf5338a2ca8230dfe3099a20bf7d
Tim L <timrdf@gmail.com>
2014-06-21 13:58:52 -0400

    unintuitive numbering; had to swap the used. added wasInformedBY

--- data/source/hcil-cs-umd-edu/IEEE-VAST-Challenge-2008-mc2/version/2008-Mar-15/convert.sh
+++ data/source/hcil-cs-umd-edu/IEEE-VAST-Challenge-2008-mc2/version/2008-Mar-15/convert.sh
@@ -4,9 +4,10 @@ mkdir -p automatic
 echo '@prefix prov: <http://www.w3.org/ns/prov#>.' > automatic/usages.ttl
 sdv=`cr-dataset-uri.sh --uri`
 for ((commit=3; commit<=1010; commit++)); do
-   let "prev=$commit-1"
-   echo "<$sdv/commit/$commit>"                >> automatic/usages.ttl
-   echo "   prov:used <$sdv/revision/$prev> ." >> automatic/usages.ttl
+   let "prev=$commit+1"
+   echo "<$sdv/commit/$commit>"                        >> automatic/usages.ttl
+   echo "   prov:used          <$sdv/revision/$prev>;" >> automatic/usages.ttl
+   echo "   prov:wasInformedBy <$sdv/commit/$prev> ."  >> automatic/usages.ttl
 done
 
 ./convert-IEEE-VAST-Challenge-2008-mc2.sh
