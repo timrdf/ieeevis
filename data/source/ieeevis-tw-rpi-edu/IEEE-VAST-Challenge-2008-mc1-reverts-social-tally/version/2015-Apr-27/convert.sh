6aea09df94277148717ffce87bccb020c9ba1124
Tim L <timrdf@gmail.com>
2015-05-06 18:48:30 -0400

    new conversion

--- data/source/ieeevis-tw-rpi-edu/IEEE-VAST-Challenge-2008-mc1-reverts-social-tally/version/2015-Apr-27/convert.sh
+++ data/source/ieeevis-tw-rpi-edu/IEEE-VAST-Challenge-2008-mc1-reverts-social-tally/version/2015-Apr-27/convert.sh
@@ -6,8 +6,15 @@
 #3> .
 #
 
+source ../../../../csv2rdf4lod-source-me-for-ieeevis.sh 
 
-mkdir -p automatic
+if [[ "$1" == "clean" ]]; then
+   echo rm -rf automatic/ ../../src/edits-absolute.vsr.xsl
+        rm -rf automatic/ ../../src/edits-absolute.vsr.xsl
+   exit
+fi
+
+mkdir -p automatic manual
 
 # Tally
 plan='https://github.com/timrdf/ieeevis/blob/master/data/source/ieeevis-tw-rpi-edu/IEEE-VAST-Challenge-2008-mc2-reverts-social-tally/src/group-by.xsl'
@@ -19,11 +26,35 @@ saxon.sh ../../src/group-by.xsl xml ttl -v `cr-sdv.sh --attribute-value` group='
 justify.sh source/reverts.rq.xml automatic/supports.ttl $plan
 
 cat automatic/against.ttl automatic/supports.ttl > automatic/against-and-supports.ttl
+echo automatic/against-and-supports.ttl
 
 # Visualize tally
 plan='https://github.com/timrdf/vsr/blob/master/src/xsl/from/template.vsr.xsl'
-vsr2grf.sh template.vsr.xsl graffle -w -od automatic  automatic/against-and-supports.ttl
+#vsr2grf.sh template.vsr.xsl graffle -w -od automatic  automatic/against-and-supports.ttl
 # TODO we collide with what it already says; append if there? : justify.sh automatic/against-and-supports.ttl automatic/against-and-supports.ttl.graffle $plan
 
-cp         'automatic'/against-and-supports.ttl.graffle 'manual'/against-and-supports.ttl.graffle
-justify.sh 'automatic'/against-and-supports.ttl.graffle 'manual'/against-and-supports.ttl.graffle 'http://dbpedia.org/resource/File_copying'
+turtle=automatic/against-and-supports.ttl
+  auto=automatic/against-and-supports.ttl.graffle
+   manual=manual/against-and-supports.ttl.graffle
+vis_strategy='../../src/edits-absolute.vsr.xsl'
+if [[ ! -e "$manual" ]]; then
+   VSR_HOME=${VSR_HOME:?"must be set."}
+   cp ../../src/edits.vsr.xsl $vis_strategy
+   # //\\
+   # TODO: push this into VSR, since already done at
+   # https://github.com/timrdf/ieeevis/blob/master/data/source/ieeevis-tw-rpi-edu/IEEE-VAST-Challenge-2008-mc1-reverts/version/2015-Apr-22/prepare.sh
+   # \\//
+   perl -pi -e "s|href=\"\.\.|href=\"$VSR_HOME/src/xsl|g" $vis_strategy
+   vsr2grf.sh $vis_strategy 'graffle' -w -od automatic/ $turtle
+   echo $auto
+
+   cp "$auto" "$manual"
+   #justify.sh $auto $manual 'file_copy'
+   # TODO we collide with what it already says; append if there? : justify.sh automatic/against-and-supports.ttl automatic/against-and-supports.ttl.graffle $plan
+   echo $manual
+else
+   echo "$manual already exists."
+fi
+
+#cp         'automatic'/against-and-supports.ttl.graffle 'manual'/against-and-supports.ttl.graffle
+#justify.sh 'automatic'/against-and-supports.ttl.graffle 'manual'/against-and-supports.ttl.graffle 'http://dbpedia.org/resource/File_copying'
