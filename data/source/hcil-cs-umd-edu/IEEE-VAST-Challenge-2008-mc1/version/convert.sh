5b245091990ce0df21e3f5ebcbfa0e3184886a52
Tim L <timrdf@gmail.com>
2015-05-07 10:42:34 -0400

    tweaking commit #s

--- data/source/hcil-cs-umd-edu/IEEE-VAST-Challenge-2008-mc1/version/convert.sh
+++ data/source/hcil-cs-umd-edu/IEEE-VAST-Challenge-2008-mc1/version/convert.sh
@@ -9,7 +9,7 @@
 mkdir -p automatic
 echo '@prefix prov: <http://www.w3.org/ns/prov#>.' > automatic/usages.ttl
 sdv=`cr-dataset-uri.sh --uri`
-for ((commit=3; commit<=1010; commit++)); do
+for ((commit=5; commit<=1013; commit++)); do
    let "prev=$commit+1"
    echo "<$sdv/commit/$commit>"                        >> automatic/usages.ttl
    echo "   prov:used          <$sdv/revision/$prev>;" >> automatic/usages.ttl
