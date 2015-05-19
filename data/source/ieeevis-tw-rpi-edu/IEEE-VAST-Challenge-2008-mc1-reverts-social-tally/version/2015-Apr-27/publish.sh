26ad40b21dedbb47940b3eff9c4ea9858bae765a
Tim L <timrdf@gmail.com>
2015-05-19 15:01:57 -0400

    justify

--- data/source/ieeevis-tw-rpi-edu/IEEE-VAST-Challenge-2008-mc1-reverts-social-tally/version/2015-Apr-27/publish.sh
+++ data/source/ieeevis-tw-rpi-edu/IEEE-VAST-Challenge-2008-mc1-reverts-social-tally/version/2015-Apr-27/publish.sh
@@ -6,8 +6,9 @@
 #3> .
 #
 
-#if [[ ! -e manual/against-and-supports.ttl.graffle.prov.ttl ]]; then
-#fi
+if [[ ! -e manual/against-and-supports.ttl.graffle.prov.ttl ]]; then
+   justify.sh automatic/against-and-supports.ttl.graffle manual/against-and-supports.ttl.graffle.prov.ttl copy_and_manual_edit
+fi
 
 for exported in against-and-supports.ttl.graffle.pdf against-and-supports.ttl.graffle.svg; do
    if [[ -e manual/$exported ]]; then
